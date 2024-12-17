8a29f9b258f14af2e51f40855fe8e40bbc08f405
Tanushka Patil <94859584+tanushkapatil@users.noreply.github.com>
2024-12-18 01:24:59 +0530

    Update BallMovement.cs

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -1,38 +1,56 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
-    public float startSpeed ;
-    public float extraSpeed ;
-    public float maxExtraSpeed ;
-    private int hitCounter ;
-    private Rigidbody2D rb ;
-    // Start is called before the first frame update
+    public float startSpeed;
+    public float extraSpeed;
+    public float maxExtraSpeed;
+    public bool playerStart = true;
+
+    private int hitCounter;
+    private Rigidbody2D rb;
+
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>() ;
-        StartCoroutine(Launch()) ;
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(Launch());
     }
 
-    public IEnumerator Launch () {
-        hitCounter = 0 ;
-        yield return new WaitForSeconds(1) ;
-        MoveBall(new Vector2(-1, 0)) ;
+    private void RestartBall()
+    {
+        rb.velocity = Vector2.zero;
+        transform.position = Vector2.zero;
     }
 
-    public void MoveBall(Vector2 direction) {
-        direction = direction.normalized ;
-        float ballSpeed = startSpeed + hitCounter * extraSpeed ;
-        rb.velocity = direction * ballSpeed ;
-    }
+    public IEnumerator Launch()
+    {
+        RestartBall();
+        hitCounter = 0;
+        yield return new WaitForSeconds(1);
 
-    public void IncreaseHitCounter() {
-        if(hitCounter * extraSpeed < maxExtraSpeed) {
-            hitCounter++ ;
+        if (playerStart)
+        {
+            MoveBall(new Vector2(-1, 0));
+        }
+        else
+        {
+            MoveBall(new Vector2(1, 0));
         }
     }
 
+    public void MoveBall(Vector2 direction)
+    {
+        direction = direction.normalized;
+        float ballSpeed = startSpeed + hitCounter * extraSpeed;
+        rb.velocity = direction * ballSpeed;
+    }
 
+    public void IncreaseHitCounter()
+    {
+        if (hitCounter * extraSpeed < maxExtraSpeed)
+        {
+            hitCounter++;
+        }
+    }
 }

f4bbf901e878ee220e34f53fd650dd912faf408f
Tanushka Patil <94859584+tanushkapatil@users.noreply.github.com>
2024-12-18 00:42:50 +0530

    Update ScoreManager.cs

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour
 {
+    public int scoreToReach ;
+
     private int player1Score = 0 ;
     private int player2Score = 0 ;
 
@@ -15,10 +18,18 @@ public class ScoreManager : MonoBehaviour
     public void Player1Goal() {
         player1Score++ ;
         player1ScoreText.text = player1Score.ToString() ;
+        CheckScore() ;
     }
 
     public void Player2Goal() {
         player2Score++ ;
         player2ScoreText.text = player2Score.ToString() ;
+        CheckScore() ;
+    }
+
+    private void CheckScore() {
+        if(player1Score == scoreToReach || player2Score == scoreToReach) {
+            SceneManager.LoadScene(2) ;
+        }
     }
 }

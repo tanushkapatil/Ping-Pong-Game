0f7cb83b504f86de5d975b46996665cb921457c1
Tanushka Patil <tanushkapatil2412@gmail.com>
2024-12-17 22:39:23 +0530

    changes

---
+++ Assets/Scripts/BallBounce.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBounce : MonoBehaviour
+{
+    public BallMovement ballMovement ;
+
+    private void Bounce(Collision2D collision) {
+        Vector3 ballPosition  = transform.position ;
+        Vector3 racketPosition = collision.transform.position ;
+        float racketHeight = collision.collider.bounds.size.y ;
+        
+        float positionX ;
+        if(collision.gameObject.name == "Player1") {
+            positionX = 1 ;
+        }
+        else {
+            positionX = -1 ;
+        }
+
+        float positionY = (ballPosition.y - racketPosition.y) / racketHeight ;
+
+        ballMovement.IncreaseHitCounter() ;
+        ballMovement.MoveBall(new Vector2(positionX, positionY)) ;
+    }
+}

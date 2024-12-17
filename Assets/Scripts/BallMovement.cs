61f4d12a37d8b41f16991bbc60138cbc1146db9e
Tanushka Patil <tanushkapatil2412@gmail.com>
2024-12-17 21:49:19 +0530

    changes

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
+    public float startSpeed ;
+    public float extraSpeed ;
+    public float maxExtraSpeed ;
+    private int hitCounter ;
+    private Rigidbody2D rb ;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>() ;
     }
+
+    public IEnumerator Launch () {
+        hitCounter = 0 ;
+        yield return new WaitForSeconds(1) ;
+    }
+    public void MoveBall(Vector2 direction) {
+        direction = direction.normalized ;
+        float ballSpeed = startSpeed + hitCounter * extraSpeed ;
+        rb.velocity = direction * ballSpeed ;
+    }
+
 
 }
